a5f668db3df25f2a33ea9e1de6848ce47ba10c8c
Abegail Reyes <abegail776@gmail.com>
2025-03-25 06:38:40 +0800

    Added services, interfaces, routes, models and api for Add Gender

---
+++ ReactNLaravelDemo/client/src/pages/gender/Genders.tsx
@@ -0,0 +1,22 @@
+import AddGenderForm from "../../components/forms/AddGenderForm";
+import GenderTable from "../../components/table/GenderTable";
+import MainLayout from "../layout/MainLayout";
+
+const Genders = () => {
+  const content = (
+    <>
+      <div className="row">
+        <div className="col-md-4">
+          <AddGenderForm />
+        </div>
+        <div className="col-md-8">
+          <GenderTable />
+        </div>
+      </div>
+    </>
+  );
+
+  return <MainLayout content={content} />;
+};
+
+export default Genders;
